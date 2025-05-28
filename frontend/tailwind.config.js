8547b3521be0788ee288f8e3c656390cd3bf1b0b
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-05-28 02:47:17 -0400

    feat: agregar encabezado con navegación y estilos, y configurar Tailwind CSS

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,js,jsx,ts,tsx,vue,mdx,md}",
+    "./src/components/**/*.{astro,js,jsx,ts,tsx,vue,mdx,md}",
+    "./src/layouts/**/*.{astro,js,jsx,ts,tsx,vue,mdx,md}",
+    "./src/pages/**/*.{astro,js,jsx,ts,tsx,vue,mdx,md}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
